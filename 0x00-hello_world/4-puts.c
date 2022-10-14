4558fb87f56165ffe74649ad877b04a681ee546a
kimo095 <eng095karim@gmail.com>
2022-10-13 18:04:04 -0700

    4pust

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,13 @@
 
 
 /**
- * main - Printing
+ * main - Printing using puts function
  *  
  * Return:Always 0 (Success)
  */
 int main(void)
 {
-   puts("\"Programming is like building a multilingual puzzle");
+   puts ("\"Programming is like building a multilingual puzzle");
    return (0);
 
 }
