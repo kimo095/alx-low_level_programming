f25649ce2a8017e6c2cd0f7ba4d6752826331978
kimo095 <eng095karim@gmail.com>
2022-10-13 17:40:18 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,16 +2,13 @@
 
 
 /**
- *  main - Entry point
+ *  main - Printing
  *  
  *  Return: Always 0 (Success)
  */
-
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
 
 }
